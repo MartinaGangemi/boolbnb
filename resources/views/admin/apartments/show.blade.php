f8f17e36c46d53086fb2c21dabf97a125ab1efaf
gianmarcodiale <98177039+gianmarcodiale@users.noreply.github.com>
2022-07-15 17:26:04 +0200

    create_admin_show_view

---
+++ resources/views/admin/apartments/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h3>{{$apartment->summary}}</h3>
+    <div class="card">
+        <img src="{{asset($apartment->cover_img)}}" alt="">
+        <div class="card-body">
+            <p>{{$apartment->description}}</p>
+            <address>{{$apartment->address}}</address>
+            <strong>Rooms: </strong><span>{{$apartment->rooms}}</span><br>
+            <strong>Beds: </strong><span>{{$apartment->beds}}</span><br>
+            <strong>Bathrooms: </strong><span>{{$apartment->bathrooms}}</span><br>
+            <strong>Square Meters: </strong><span>{{$apartment->square_meters}}</span><br>
+            <cite>{{$apartment->slug}}</cite>
+        </div>
+    </div>
+</div>
+@endsection
