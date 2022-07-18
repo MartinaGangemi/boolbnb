56e7b95c52abb0bb207ccdf0a35a222b4116fa08
gianmarcodiale <98177039+gianmarcodiale@users.noreply.github.com>
2022-07-18 12:07:04 +0200

    fix valori del checkbox non si salvavano

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-<section id="apartment-form" class="p-3 bg-container">
+    <section id="apartment-form" class="p-3 bg-container">
         <div class="container">
             <h4 class="text-center">Insert your apartment's info.</h4>
 
@@ -17,27 +17,32 @@
                         </div>
                     @endif
 
-                    <form class="p-2 my_form" action="{{route('admin.apartments.update', $apartment->slug)}}" method="POST" enctype="multipart/form-data">
+                    <form class="p-2 my_form" action="{{ route('admin.apartments.update', $apartment->slug) }}" method="POST"
+                        enctype="multipart/form-data">
                         @csrf
                         @method ('patch')
 
 
                         <div>
                             <label for="summary" class="form-label">Title</label>
-                            <input type="text" class="form-control" id="summary" name="summary" placeholder="Add a summary" value="{{old('summary', $apartment->summary)}}">
+                            <input type="text" class="form-control" id="summary" name="summary"
+                                placeholder="Add a summary" value="{{ old('summary', $apartment->summary) }}">
                         </div>
 
                         <div class="row mt-4 ms-2">
                             <div class="form-check col-sm-12 col-lg-6 mb-3">
                                 <input class="form-check-input" type="hidden" value="0" id="visible" name="visible">
-                                <input class="form-check-input" type="checkbox" value="1" id="visible" name="visible">
-                                <label class="form-check-label yellow-label" for="visible">Check this if you want your apartment to be visibile</label>
+                                <input class="form-check-input" type="checkbox" value="1" id="visible"
+                                    name="visible">
+                                <label class="form-check-label yellow-label" for="visible">Check this if you want your
+                                    apartment to be visibile</label>
                             </div>
 
                             <div class="form-group col-sm-12 col-lg-6">
                                 <div class="dropdown">
 
-                                    <button class="btn btn-custom dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <button class="btn btn-custom dropdown-toggle" type="button" id="dropdownMenu2"
+                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         Choose your services
                                     </button>
 
@@ -45,9 +50,11 @@
                                         <button class="my_button dropdown-item" type="button">
                                             @foreach ($services as $service)
                                                 <div>
-                                                    <input type="checkbox" class="form-check-input" id="{{ $service->id }}" value="{{$service->id}}" name="services[]"
-                                                        @if(in_array($service->id, old('services', []))) checked @endif>
-                                                    <label class="form-check-label" for="{{$service->id}}">{{$service->name}}</label>
+                                                    <input type="checkbox" class="form-check-input"
+                                                        id="{{ $service->id }}" value="{{ $service->id }}"
+                                                        name="services[]" @if ($apartment->services->contains($service->id)) checked @endif>
+                                                    <label class="form-check-label"
+                                                        for="{{ $service->id }}">{{ $service->name }}</label>
                                                 </div>
                                             @endforeach
                                         </button>
@@ -60,33 +67,37 @@
                         <div class="row mt-3">
                             <div class="col-sm-12 col-lg-6">
                                 <label for="cover_img" class="form-label">Image</label>
-                                <input type="file" class="form-control" placeholder="Choose an cover_img" id="cover_img" name="cover_img" value="{{old('cover_img', $apartment->cover_img)}}">
+                                <input type="file" class="form-control" placeholder="Choose an cover_img" id="cover_img"
+                                    name="cover_img" value="{{ old('cover_img', $apartment->cover_img) }}">
                             </div>
 
                             <div class="col-sm-12 col-lg-6">
                                 <label for="description" class="form-label">Description</label>
-                                <textarea class="form-control" id="description" name="description" placeholder="Add a description">{{old('description', $apartment->description)}}</textarea>
+                                <textarea class="form-control" id="description" name="description" placeholder="Add a description">{{ old('description', $apartment->description) }}</textarea>
                             </div>
                         </div>
 
                         <div class="row mt-3">
                             <div class=" col-sm-12 col-lg-6">
                                 <label for="address" class="form-label">address</label>
-                                <input class="form-control" id="address" name="address" value="{{old('address',  $apartment->address)}}" placeholder="Choose a address">
+                                <input class="form-control" id="address" name="address"
+                                    value="{{ old('address', $apartment->address) }}" placeholder="Choose a address">
                             </div>
 
 
-                                <div class=" col-sm-12 col-lg-6">
-                                    <label for="lat" class="form-label">lat</label>
-                                    <input class="form-control" id="lat" name="lat" value="{{old('lat',  $apartment->lat)}}" placeholder="Choose a lat">
-                                </div>
+                            <div class=" col-sm-12 col-lg-6">
+                                <label for="lat" class="form-label">lat</label>
+                                <input class="form-control" id="lat" name="lat"
+                                    value="{{ old('lat', $apartment->lat) }}" placeholder="Choose a lat">
+                            </div>
 
 
 
-                              <div class=" col-sm-12 col-lg-6">
-                                        <label for="lon" class="form-label">lon</label>
-                                        <input class="form-control" id="lon" name="lon" value="{{old('lon',  $apartment->lon)}}" placeholder="Choose a lon">
-                              </div>
+                            <div class=" col-sm-12 col-lg-6">
+                                <label for="lon" class="form-label">lon</label>
+                                <input class="form-control" id="lon" name="lon"
+                                    value="{{ old('lon', $apartment->lon) }}" placeholder="Choose a lon">
+                            </div>
 
 
 
@@ -97,36 +108,43 @@
                         <div class="row mt-3">
                             <div class=" col-sm-12 col-lg-6">
                                 <label for="rooms" class="form-label">Rooms</label>
-                                <input type="text" class="form-control" id="rooms" name="rooms" value="{{old('rooms', $apartment->rooms)}}" placeholder="Add your rooms' number">
+                                <input type="text" class="form-control" id="rooms" name="rooms"
+                                    value="{{ old('rooms', $apartment->rooms) }}" placeholder="Add your rooms' number">
                             </div>
 
                             <div class=" col-sm-12 col-lg-6">
                                 <label for="beds" class="form-label">Beds</label>
-                                <input type="text" class="form-control" id="beds" name="beds" value="{{old('beds', $apartment->beds)}}" placeholder="Add your beds' number">
+                                <input type="text" class="form-control" id="beds" name="beds"
+                                    value="{{ old('beds', $apartment->beds) }}" placeholder="Add your beds' number">
                             </div>
                         </div>
 
                         <div class="row mt-3">
                             <div class=" col-sm-12 col-lg-6">
                                 <label for="bathrooms" class="form-label">Bathrooms</label>
-                                <input type="text" class="form-control" id="bathrooms" name="bathrooms" value="{{old('bathrooms', $apartment->bathrooms)}}" placeholder="Add your bathrooms' number">
+                                <input type="text" class="form-control" id="bathrooms" name="bathrooms"
+                                    value="{{ old('bathrooms', $apartment->bathrooms) }}"
+                                    placeholder="Add your bathrooms' number">
                             </div>
 
                             <div class=" col-sm-12 col-lg-6">
                                 <label for="square_meters" class="form-label">Square Meters</label>
-                                <input type="text" class="form-control" id="square_meters" name="square_meters" value="{{old('square_meters', $apartment->square_meters)}}" placeholder="Add your square meters number">
+                                <input type="text" class="form-control" id="square_meters" name="square_meters"
+                                    value="{{ old('square_meters', $apartment->square_meters) }}"
+                                    placeholder="Add your square meters number">
                             </div>
                         </div>
                         {{-- Questo deve stare qui se no il form non funziona --}}
-                        <button type="submit" class="btn btn-success mt-5 w-100" >Edit your apartment</button>
+                        <button type="submit" class="btn btn-success mt-5 w-100">Edit your apartment</button>
                     </form>
 
 
 
                 </div>
             </div>
-            <h5 class="text-center"><a href="{{ route('admin.apartments.index') }}" class="font-italic">Click here to go back to your apartments.</a></h5>
+            <h5 class="text-center"><a href="{{ route('admin.apartments.index') }}" class="font-italic">Click here to
+                    go back to your apartments.</a></h5>
 
         </div>
-</section>
+    </section>
 @endsection
