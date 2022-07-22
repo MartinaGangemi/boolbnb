d745b466a81606d4ace4ad383b6064e6544a674b
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-07-22 18:19:17 +0200

    Add 20 km radius search

--- resources/js/Pages/Search.vue
+++ resources/js/Pages/Search.vue
@@ -93,75 +93,116 @@ export default {
       addressResults: [],
       lat: 0,
       lon: 0,
+      searchLat: 0,
+      searchLon: 0,
+      defaultDistance: 20
 
-      
     };
   },
 
-
-
   methods: {
-    searchApartments(addressId) {
+
+    searchApartments() {
+
       this.apartments = [];
+
       axios
         .get("/api/apartments")
         .then((response) => {
+
           //console.log(response.data);
           const results = response.data.data;
           this.apartmentsResponse = response.data;
-          results.forEach(result => {
-            if (result.address.includes(this.searchText)) {
-              this.apartments.push(result);
-            }
+
+          const link = 'https://kr-api.tomtom.com/search/2/geocode/' + this.searchText + '.json?key=D4OSGfRW4VAQYImcVowdausckQhvMUbq&typeahead=true';
+
+          axios
+            .get(link)
+            .then((searchResponse) => {
+
+              let searchResults = searchResponse.data.results;
+
+              //console.log('Risultati di ricerca: ' , searchResults[0].position);
+              this.searchLat = searchResults[0].position.lat;
+              this.searchLon = searchResults[0].position.lon;
+
+              results.forEach((result) => {
+
+                //console.log("Risultato: ", result);
+
+                const distance = this.getDistanceFromLatLonInKm(result.lat, result.lon, this.searchLat, this.searchLon);
+                //console.log(distance);
+
+                if (distance <= this.defaultDistance) {
+                  this.apartments.push(result);
+                }
+
+              });
+
+              //mappa
+              this.createMap();
+
+              this.searchText = '';
+
+            });           
+          
+          })
+          .catch((e) => {
+            console.error(e);
           });
 
+    },
 
-  //mappa
-  this.createMap()
-    
+    getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+      let R = 6371; // Radius of the earth in km
+      let dLat = this.deg2rad(lat2 - lat1); // deg2rad below
+      let dLon = this.deg2rad(lon2 - lon1);
+      let a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+      let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      let d = R * c; // Distance in km
+          return d;
+    },
 
-    this.searchText = '';
-    })
-    .catch((e) => {
-      console.error(e);
+    deg2rad(deg) {
+      return deg * (Math.PI / 180);
+    },
+
+    createMap(){
+      let map = tt.map({
+      key: 'D4OSGfRW4VAQYImcVowdausckQhvMUbq',
+      container:  'map',
+      style: 'tomtom://vector/1/basic-main',
+      center: [this.apartments[0].lon,this.apartments[0].lat],
+      zoom: 17
+      });
+
+      map.addControl(new tt.FullscreenControl());
+      map.addControl(new tt.NavigationControl());
+
+      let popupOffset = 25;
+
+
+      this.apartments.forEach(apartment=>{
+
+      let latMarker = apartment.lat;
+      let lonMarker = apartment.lon;
+      //  let lat = this.apartments[0].lat
+      //  let lon = this.apartments[0].lon
+        let coordinates = [lonMarker, latMarker]
+        console.log(coordinates)
+
+        //marker
+
+        let marker = new tt.Marker().setLngLat(coordinates).addTo(map);
+        console.log(marker)
+        let popup = new tt.Popup({ offset: popupOffset }).setHTML(apartment.summary);
+        marker.setPopup(popup).togglePopup();
+
     });
-
-  },
-
-  createMap(){
-    let map = tt.map({
-    key: 'D4OSGfRW4VAQYImcVowdausckQhvMUbq',
-    container:  'map',
-    style: 'tomtom://vector/1/basic-main',
-    center: [this.apartments[0].lon,this.apartments[0].lat],
-    zoom: 17
-    });
-
-    map.addControl(new tt.FullscreenControl());
-    map.addControl(new tt.NavigationControl());
-
-    let popupOffset = 25;
-
-
-    this.apartments.forEach(apartment=>{
-
-    let latMarker = apartment.lat;
-    let lonMarker = apartment.lon;
-    //  let lat = this.apartments[0].lat
-    //  let lon = this.apartments[0].lon
-      let coordinates = [lonMarker, latMarker]
-      console.log(coordinates)
-
-      //marker
-
-      let marker = new tt.Marker().setLngLat(coordinates).addTo(map);
-      console.log(marker)
-      let popup = new tt.Popup({ offset: popupOffset }).setHTML(apartment.summary);
-      marker.setPopup(popup).togglePopup();
-
-  });
-  },
-
+    },
 
     searchAddress() {
       window.axios.defaults.headers.common = {
@@ -198,9 +239,7 @@ export default {
       //nasconde la lista degli indirizzi/cittò
        this.isHidden = true
       
-    },
-
-   
+    },   
 
     //end methods
   },
@@ -217,135 +256,7 @@ export default {
 
 
 };
-</script>
 
-export default {
-  name: "Search",
-
-  data() {
-    return {
-    
-      apartments: [],
-      apartmentsResponse: "",
-      searchText: "",
-
-      addressResults: [],
-      lat: 0,
-      lon: 0,
-
-      
-    };
-  },
-
-
-
-  methods: {
-    searchApartments(addressId) {
-      this.apartments = [];
-      axios
-        .get("/api/apartments")
-        .then((response) => {
-          //console.log(response.data);
-          const results = response.data.data;
-          this.apartmentsResponse = response.data;
-          results.forEach(result => {
-            if (result.address.includes(this.searchText)) {
-              this.apartments.push(result);
-            }
-          });
-
-
-  //mappa
-
-    let map = tt.map({
-    key: 'D4OSGfRW4VAQYImcVowdausckQhvMUbq',
-    container:  'map',
-    style: 'tomtom://vector/1/basic-main',
-    center: [this.apartments[0].lon,this.apartments[0].lat],
-    zoom: 17
-    });
-
-    map.addControl(new tt.FullscreenControl());
-    map.addControl(new tt.NavigationControl());
-
-    let popupOffset = 25;
-
-
-    this.apartments.forEach(apartment=>{
-
-    let latMarker = apartment.lat;
-    let lonMarker = apartment.lon;
-    //  let lat = this.apartments[0].lat
-    //  let lon = this.apartments[0].lon
-      let coordinates = [lonMarker, latMarker]
-      console.log(coordinates)
-
-      //marker
-
-          let marker = new tt.Marker().setLngLat(coordinates).addTo(map);
-          console.log(marker)
-          let popup = new tt.Popup({ offset: popupOffset }).setHTML(apartment.summary);
-          marker.setPopup(popup).togglePopup();
-
-  });
-
-    this.searchText = '';
-    })
-    .catch((e) => {
-      console.error(e);
-    });
-
-  },
-
-
-    searchAddress() {
-      window.axios.defaults.headers.common = {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      };
-
-      //const resultElement = document.querySelector('.results')
-      //resultElement.innerHTML = ''
-      const link =
-        `https://kr-api.tomtom.com/search/2/geocode/` +
-        this.searchText +
-        `.json?key=D4OSGfRW4VAQYImcVowdausckQhvMUbq&typeahead=true`;
-      axios.get(link).then((response) => {
-        let results = response.data.results;
-        //console.log(results);
-        this.addressResults = results;
-      });
-      //visualizza la lista degli indirizzi/città
-      this.isHidden = false
-    },
-
-    checkAddress(addressId) {
-      
-      this.searchText = null;
-     
-      console.log(addressId);
-      console.log(this.addressResults[0].address.freeformAddress);
-
-      //prende la lista delle città e lat e lon
-      this.searchText = this.addressResults[addressId].address.freeformAddress;
-      this.lat = this.addressResults[addressId].position.lat;
-      this.lon = this.addressResults[addressId].position.lon;
-      //nasconde la lista degli indirizzi/cittò
-       this.isHidden = true
-      
-    },
-
-   
-
-    //end methods
-  },
-
-  created(){
-    this.apartments=this.$route.params.data
-  }
-
-
-};
 </script>
 
 <style lang="scss" scoped>
